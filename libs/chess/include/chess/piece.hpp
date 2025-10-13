8fed0bf2769f8338b57fd15690f73db529261a24
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-10-13 23:48:04 +0530

    remove usage of unnamed namespace

--- libs/chess/include/chess/piece.hpp
+++ libs/chess/include/chess/piece.hpp
@@ -15,7 +15,7 @@ enum class PieceType : std::uint8_t {
   KING,
 };
 
-namespace {
+namespace internal {
 
 constexpr std::uint8_t PIECE_COLOR_SHIFT = 4;
 constexpr std::uint8_t PIECE_TYPE_MASK = 0x0F;
@@ -26,35 +26,42 @@ constexpr std::uint8_t get_piece_encoded_value(Color piece_color,
          static_cast<std::uint8_t>(piece_type);
 }
 
-}  // namespace
+}  // namespace internal
 
 enum class Piece : std::uint8_t {
-  WHITE_PAWN = get_piece_encoded_value(Color::WHITE, PieceType::PAWN),
-  WHITE_KNIGHT = get_piece_encoded_value(Color::WHITE, PieceType::KNIGHT),
-  WHITE_BISHOP = get_piece_encoded_value(Color::WHITE, PieceType::BISHOP),
-  WHITE_ROOK = get_piece_encoded_value(Color::WHITE, PieceType::ROOK),
-  WHITE_QUEEN = get_piece_encoded_value(Color::WHITE, PieceType::QUEEN),
-  WHITE_KING = get_piece_encoded_value(Color::WHITE, PieceType::KING),
-  BLACK_PAWN = get_piece_encoded_value(Color::BLACK, PieceType::PAWN),
-  BLACK_KNIGHT = get_piece_encoded_value(Color::BLACK, PieceType::KNIGHT),
-  BLACK_BISHOP = get_piece_encoded_value(Color::BLACK, PieceType::BISHOP),
-  BLACK_ROOK = get_piece_encoded_value(Color::BLACK, PieceType::ROOK),
-  BLACK_QUEEN = get_piece_encoded_value(Color::BLACK, PieceType::QUEEN),
-  BLACK_KING = get_piece_encoded_value(Color::BLACK, PieceType::KING),
+  WHITE_PAWN = internal::get_piece_encoded_value(Color::WHITE, PieceType::PAWN),
+  WHITE_KNIGHT =
+      internal::get_piece_encoded_value(Color::WHITE, PieceType::KNIGHT),
+  WHITE_BISHOP =
+      internal::get_piece_encoded_value(Color::WHITE, PieceType::BISHOP),
+  WHITE_ROOK = internal::get_piece_encoded_value(Color::WHITE, PieceType::ROOK),
+  WHITE_QUEEN =
+      internal::get_piece_encoded_value(Color::WHITE, PieceType::QUEEN),
+  WHITE_KING = internal::get_piece_encoded_value(Color::WHITE, PieceType::KING),
+  BLACK_PAWN = internal::get_piece_encoded_value(Color::BLACK, PieceType::PAWN),
+  BLACK_KNIGHT =
+      internal::get_piece_encoded_value(Color::BLACK, PieceType::KNIGHT),
+  BLACK_BISHOP =
+      internal::get_piece_encoded_value(Color::BLACK, PieceType::BISHOP),
+  BLACK_ROOK = internal::get_piece_encoded_value(Color::BLACK, PieceType::ROOK),
+  BLACK_QUEEN =
+      internal::get_piece_encoded_value(Color::BLACK, PieceType::QUEEN),
+  BLACK_KING = internal::get_piece_encoded_value(Color::BLACK, PieceType::KING),
 };
 
 constexpr Piece get_piece(Color piece_color, PieceType piece_type) {
-  return static_cast<Piece>(get_piece_encoded_value(piece_color, piece_type));
+  return static_cast<Piece>(
+      internal::get_piece_encoded_value(piece_color, piece_type));
 }
 
 constexpr Color get_piece_color(Piece piece) {
   return static_cast<Color>(static_cast<std::uint8_t>(piece) >>
-                            PIECE_COLOR_SHIFT);
+                            internal::PIECE_COLOR_SHIFT);
 }
 
 constexpr PieceType get_piece_type(Piece piece) {
   return static_cast<PieceType>(static_cast<std::uint8_t>(piece) &
-                                PIECE_TYPE_MASK);
+                                internal::PIECE_TYPE_MASK);
 }
 
 }  // namespace chess
