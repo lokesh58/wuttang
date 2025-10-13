550be77e89d7b58da07dfc0b9ad9525a563b0df0
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-10-14 00:50:30 +0530

    mark functions which do not throw as noexcept

--- libs/chess/include/chess/piece.hpp
+++ libs/chess/include/chess/piece.hpp
@@ -21,7 +21,7 @@ constexpr std::uint8_t PIECE_COLOR_SHIFT = 4;
 constexpr std::uint8_t PIECE_TYPE_MASK = 0x0F;
 
 constexpr std::uint8_t get_piece_encoded_value(Color piece_color,
-                                               PieceType piece_type) {
+                                               PieceType piece_type) noexcept {
   return (static_cast<std::uint8_t>(piece_color) << PIECE_COLOR_SHIFT) |
          static_cast<std::uint8_t>(piece_type);
 }
@@ -49,17 +49,17 @@ enum class Piece : std::uint8_t {
   BLACK_KING = internal::get_piece_encoded_value(Color::BLACK, PieceType::KING),
 };
 
-constexpr Piece get_piece(Color piece_color, PieceType piece_type) {
+constexpr Piece get_piece(Color piece_color, PieceType piece_type) noexcept {
   return static_cast<Piece>(
       internal::get_piece_encoded_value(piece_color, piece_type));
 }
 
-constexpr Color get_piece_color(Piece piece) {
+constexpr Color get_piece_color(Piece piece) noexcept {
   return static_cast<Color>(static_cast<std::uint8_t>(piece) >>
                             internal::PIECE_COLOR_SHIFT);
 }
 
-constexpr PieceType get_piece_type(Piece piece) {
+constexpr PieceType get_piece_type(Piece piece) noexcept {
   return static_cast<PieceType>(static_cast<std::uint8_t>(piece) &
                                 internal::PIECE_TYPE_MASK);
 }
