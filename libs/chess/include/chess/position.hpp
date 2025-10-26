9a141555b511cc9b3c4cc6b0ae60b33ab1f06bbe
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-10-26 22:32:13 +0530

    add creation methods for Position class

--- libs/chess/include/chess/position.hpp
+++ libs/chess/include/chess/position.hpp
@@ -16,11 +16,15 @@ namespace chess {
 
 class Position {
 public:
-    Position();
+    static constexpr std::string_view STANDARD_STARTING_FEN =
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
+    static Position standard();
     static Position from_fen(std::string_view fen_string);
 
 private:
+    Position() noexcept;
+
     static constexpr std::size_t BOARD_SIZE = 64;
     using Board = std::array<std::optional<Piece>, BOARD_SIZE>;
 
@@ -28,15 +32,16 @@ private:
         Move move;
         std::optional<Square> en_passant_square;
         CastlingRights castling_rights;
-        std::uint8_t half_move_counter;
+        std::uint8_t half_move_clock;
     };
 
     Board board_;
     Color side_to_move_;
     std::optional<Square> en_passant_square_;
     CastlingRights castling_rights_;
-    std::uint8_t half_move_counter_;
+    std::uint8_t half_move_clock_;
     std::vector<History> history_;
 };
 
 }  // namespace chess
+
--- libs/chess/src/position.cpp
+++ libs/chess/src/position.cpp
@@ -1,1 +1,24 @@
-#include <chess/position.hpp>
+#include "chess/position.hpp"
+
+#include <stdexcept>
+
+#include "chess/castling_rights.hpp"
+#include "chess/color.hpp"
+
+namespace chess {
+
+Position Position::standard() {
+    return Position::from_fen(STANDARD_STARTING_FEN);
+}
+
+Position Position::from_fen(std::string_view) {
+    // TODO: implement FEN parsing
+    throw std::runtime_error("Not implemented");
+}
+
+Position::Position() noexcept:
+        side_to_move_(Color::WHITE),
+        castling_rights_(CastlingRights::NONE) {};
+
+}  // namespace chess
+
