dfee9d20034115e498a0244b874f0384ff46efc6
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-10-30 11:02:29 +0530

    use detail namespace

--- libs/chess/include/chess/piece.hpp
+++ libs/chess/include/chess/piece.hpp
@@ -15,12 +15,12 @@ enum class PieceType : std::uint8_t {
     KING,
 };
 
-namespace {
+namespace detail {
 
     constexpr std::uint8_t PIECE_COLOR_SHIFT = 4;
     constexpr std::uint8_t PIECE_TYPE_MASK = 0x0F;
 
-    constexpr std::uint8_t get_piece_encoded_value(
+    inline constexpr std::uint8_t get_piece_encoded_value(
         Color piece_color,
         PieceType piece_type
     ) noexcept {
@@ -28,39 +28,47 @@ namespace {
                static_cast<std::uint8_t>(piece_type);
     }
 
-}  // namespace
+}  // namespace detail
 
 enum class Piece : std::uint8_t {
-    WHITE_PAWN = get_piece_encoded_value(Color::WHITE, PieceType::PAWN),
-    WHITE_KNIGHT = get_piece_encoded_value(Color::WHITE, PieceType::KNIGHT),
-    WHITE_BISHOP = get_piece_encoded_value(Color::WHITE, PieceType::BISHOP),
-    WHITE_ROOK = get_piece_encoded_value(Color::WHITE, PieceType::ROOK),
-    WHITE_QUEEN = get_piece_encoded_value(Color::WHITE, PieceType::QUEEN),
-    WHITE_KING = get_piece_encoded_value(Color::WHITE, PieceType::KING),
-    BLACK_PAWN = get_piece_encoded_value(Color::BLACK, PieceType::PAWN),
-    BLACK_KNIGHT = get_piece_encoded_value(Color::BLACK, PieceType::KNIGHT),
-    BLACK_BISHOP = get_piece_encoded_value(Color::BLACK, PieceType::BISHOP),
-    BLACK_ROOK = get_piece_encoded_value(Color::BLACK, PieceType::ROOK),
-    BLACK_QUEEN = get_piece_encoded_value(Color::BLACK, PieceType::QUEEN),
-    BLACK_KING = get_piece_encoded_value(Color::BLACK, PieceType::KING),
+    WHITE_PAWN = detail::get_piece_encoded_value(Color::WHITE, PieceType::PAWN),
+    WHITE_KNIGHT =
+        detail::get_piece_encoded_value(Color::WHITE, PieceType::KNIGHT),
+    WHITE_BISHOP =
+        detail::get_piece_encoded_value(Color::WHITE, PieceType::BISHOP),
+    WHITE_ROOK = detail::get_piece_encoded_value(Color::WHITE, PieceType::ROOK),
+    WHITE_QUEEN =
+        detail::get_piece_encoded_value(Color::WHITE, PieceType::QUEEN),
+    WHITE_KING = detail::get_piece_encoded_value(Color::WHITE, PieceType::KING),
+    BLACK_PAWN = detail::get_piece_encoded_value(Color::BLACK, PieceType::PAWN),
+    BLACK_KNIGHT =
+        detail::get_piece_encoded_value(Color::BLACK, PieceType::KNIGHT),
+    BLACK_BISHOP =
+        detail::get_piece_encoded_value(Color::BLACK, PieceType::BISHOP),
+    BLACK_ROOK = detail::get_piece_encoded_value(Color::BLACK, PieceType::ROOK),
+    BLACK_QUEEN =
+        detail::get_piece_encoded_value(Color::BLACK, PieceType::QUEEN),
+    BLACK_KING = detail::get_piece_encoded_value(Color::BLACK, PieceType::KING),
 };
 
 inline constexpr Piece get_piece(
     Color piece_color,
     PieceType piece_type
 ) noexcept {
-    return static_cast<Piece>(get_piece_encoded_value(piece_color, piece_type));
+    return static_cast<Piece>(
+        detail::get_piece_encoded_value(piece_color, piece_type)
+    );
 }
 
 inline constexpr Color get_piece_color(Piece piece) noexcept {
     return static_cast<Color>(
-        static_cast<std::uint8_t>(piece) >> PIECE_COLOR_SHIFT
+        static_cast<std::uint8_t>(piece) >> detail::PIECE_COLOR_SHIFT
     );
 }
 
 inline constexpr PieceType get_piece_type(Piece piece) noexcept {
     return static_cast<PieceType>(
-        static_cast<std::uint8_t>(piece) & PIECE_TYPE_MASK
+        static_cast<std::uint8_t>(piece) & detail ::PIECE_TYPE_MASK
     );
 }
 
