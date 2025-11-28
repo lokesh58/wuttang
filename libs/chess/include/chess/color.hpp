c0ea9d923d5006e2c9a9665ba22b1fd6377100f4
Lokesh Patel <32188450+lokesh58@users.noreply.github.com>
2025-11-29 01:07:35 +0530

    update color values

--- libs/chess/include/chess/color.hpp
+++ libs/chess/include/chess/color.hpp
@@ -5,12 +5,17 @@
 namespace chess {
 
 enum class Color : std::uint8_t {
-    WHITE,
-    BLACK,
+    NONE = 0,
+    WHITE = 1 << 4,
+    BLACK = 1 << 5,
+    BOTH = WHITE | BLACK,
 };
 
 inline constexpr Color invert(Color color) {
-    return static_cast<Color>(static_cast<std::uint8_t>(color) ^ 1);
+    return static_cast<Color>(
+        static_cast<std::uint8_t>(color) ^
+        static_cast<std::uint8_t>(Color::BOTH)
+    );
 }
 
 }  // namespace chess
